ded0b4005a31d38af433876658b26559a22e2557
Srinidhichaturvede <58887345+Srinidhichaturvede@users.noreply.github.com>
2020-07-24 11:53:48 +0530

    Update BookController.java

--- Downloads_bookapi/src/main/java/com/demo/bookapi/controller/BookController.java
+++ Downloads_bookapi/src/main/java/com/demo/bookapi/controller/BookController.java
@@ -29,8 +29,10 @@ public class BookController {
 	@Autowired
 	BookRepository bookRepository;
 	
-	
-
+	@GetMapping("/book")
+	public List<Book> getAllBooks() {
+	    return bookRepository.findAll();
+	}
 	
 	@PostMapping("/book")
 	public Book createBook(@Valid @RequestBody Book book) {
